013ae385b261ec7e3ad8020d9f6ba441c4661395
María Arévalo <mi.arevalo10@uniandes.edu.co>
2017-05-26 13:09:48 -0500

    bug thumbnail

--- imports/api/bbyProducts.js
+++ imports/api/bbyProducts.js
@@ -13,11 +13,12 @@ if(Meteor.isClient){
 }
     Meteor.methods({
       searchBby(query,categoria){
+        console.log("id en bby", id);
         Products.remove({userId: id});
         console.log("category id", categoria);
         console.log("getprd");
         let bby = require('bestbuy')(process.env.BBY_API_KEY);
-        bby.products('(search='+query+')&(categoryPath.id=' +categoria+')', {show: 'name,salePrice,shippingCost,url,shipping,customerReviewAverage,description', pageSize: 10})
+        bby.products('(search='+query+')&(categoryPath.id=' +categoria+')', {show: 'name,salePrice,shippingCost,url,shipping,customerReviewAverage,description,thumbnailImage', pageSize: 10})
           .then(Meteor.bindEnvironment(function(data){
             if (data.total === 0) console.log('No products found');
             else{
